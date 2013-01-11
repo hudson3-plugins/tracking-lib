13a4badf65d5a2d6b78e3989cb0d85ff50074e1c
Bob Foster <bobfoster@gmail.com>
2013-01-11 14:15:18 -0800

    Use/fix proxy

--- read_update_center.py
+++ read_update_center.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python
 #
 
-import urllib, json, sys
+import urllib, json, sys, os
 
 def read_update_center(url):
-	file = urllib.urlopen(url)
+	proxymap = {}
+	proxy = os.environ.get('http_proxy', None)
+	if proxy:
+		if not proxy.startswith("http:") and not proxy.startswith("https:"):
+			proxy = "http://"+proxy
+		proxymap['http'] = proxy
+	file = urllib.urlopen(url, None, proxymap)
 	text = file.read()
 	file.close()
 	if text.startswith('updateCenter.post(') and text.endswith(');'):
