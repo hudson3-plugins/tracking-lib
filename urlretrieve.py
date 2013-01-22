8a6d3f3baf73f842a2e0776a66cb703b89065f1d
Bob Foster <bobfoster@gmail.com>
2013-01-21 16:10:31 -0800

    Add exception handling to urlretrieve

--- urlretrieve.py
+++ urlretrieve.py
@@ -12,19 +12,23 @@ from read_update_center import get_proxy_map
 def urlretrieve(url, path):
 	proxy_handler = urllib2.ProxyHandler(get_proxy_map())
 	opener = urllib2.build_opener(proxy_handler)
-	input = opener.open(url)
-	output = open(path, 'wb')
 	try:
-		output.write(input.read())
-	finally:
-		output.close()
-		input.close()
+		input = opener.open(url)
+		output = open(path, 'wb')
+		try:
+			output.write(input.read())
+			return True
+		finally:
+			output.close()
+			input.close()
+	except:
+		print 'urlretrieve("'+url+'", "'+path+'" FAILED'
+		return False
 
 if __name__ == "__main__":
 	if len(sys.argv) != 3:
 		print "Usage: ./urlretrieve URL FILE_PATH"
 		sys.exit(1)
-	print sys.argv[1], sys.argv[2]
 	urlretrieve(sys.argv[1], sys.argv[2])
 
 		
